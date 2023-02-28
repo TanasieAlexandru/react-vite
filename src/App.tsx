746f4e7b32e8733f976d547ee6727512b9146d53
TanasieAlexandru <alexandru_tanasie@yahoo.com>
2023-02-28 15:19:49 +0200

    added jotai library and refactored routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home/Home';
 import TicTacToeGame from './components/TicTacToe/TicTacToe';
+import Jotai from './components/Jotai/Jotai';
+
+const routes = {
+	home: '/',
+	ticTacToeGame: '/ticTacToeGame',
+	jotai: '/jotai',
+};
 
 export default function App() {
 	return (
@@ -9,15 +16,19 @@ export default function App() {
 			<div className="App">
 				<ul>
 					<li>
-						<Link to="/">Home</Link>
+						<Link to={routes.home}>Home</Link>
 					</li>
 					<li>
-						<Link to="/ticTacToeGame">TicTacToeGame</Link>
+						<Link to={routes.ticTacToeGame}>TicTacToeGame</Link>
+					</li>
+					<li>
+						<Link to={routes.jotai}>Jotai example</Link>
 					</li>
 				</ul>
 				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/ticTacToeGame" element={<TicTacToeGame />}></Route>
+					<Route path={routes.home} element={<Home />}></Route>
+					<Route path={routes.ticTacToeGame} element={<TicTacToeGame />}></Route>
+					<Route path={routes.jotai} element={<Jotai />}></Route>
 				</Routes>
 			</div>
 		</Router>
